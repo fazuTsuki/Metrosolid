c8e98ffd27504c0afc6fc8f072832f7c6d4058e4
Davin_UPI <davin1fausta@upi.edu>
2024-01-23 11:51:38 +0700

    Enemy ver 0.0.3
    Changelog:
    + Redo logic for engagement and damage calculation
    + modify the player skill Resource to accomodate more variables

--- Scenes/Components/EngagementComponent/EngagementComponent.gd
+++ Scenes/Components/EngagementComponent/EngagementComponent.gd
@@ -5,30 +5,23 @@ A more specific point system for engagement  stat
 class_name EngagementComponent
 extends GenericPointComponent
 
-enum STATUS{
-	BORED,
-	NORMAL,
-	LAUGH
-}
+@export var treshold : float = 20
 
-#A Signal when the status changed, duh
-signal status_changed
-
-var current_status : STATUS = STATUS.BORED :
-	set(value):
-		if value != current_status:
-			current_status = value
-			status_changed.emit()
+var old_points : float
 
 func add_points(delta : float):
 	if delta != 0 :
+		old_points = current_points
 		points_added.emit() if delta > 0 else points_deducted.emit()
 		current_points = clampf(current_points + delta, min_point, max_point)
 		
 		if current_points == max_point : points_full.emit() 
 		elif current_points == min_point : points_depleted.emit()
-		
-		# < 60% = bored; >60% and <100% = normal; 100% = laugh
-		if current_points <= 0.6 : current_status = STATUS.BORED
-		elif current_points > 0.6 and current_points < 1 : current_status = STATUS.NORMAL
-		else : current_status = STATUS.LAUGH
+	
+func deduct_point(delta : float):
+	add_points(-delta)
+
+func check_treshold() -> bool :
+	var result = current_points - old_points >= treshold
+	old_points = 0
+	return result
